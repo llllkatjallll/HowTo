02305731dfe3003d24b06d1fe99266075a61b0fb
katja.rempel <rempel@prefrontalcortex.de>
2021-03-17 17:52:56 +0100

    copied website for MvB version

---
+++ 210314_Boch/games/natur/natur.js
@@ -0,0 +1,461 @@
+var capture,captureHeight,canvas;
+let switchFlag = false;
+let shootBtn, newCaptureBtn, invertBtn,galleryBtn,backBtn;
+let slider1;
+let img1,img2,img3;
+var imgs = [];
+var w, h;
+var picture;
+var options = {
+  video: {
+
+    facingMode: {
+      // exact: "environment"
+     // exact: "user"
+    },
+    // mandatory: {
+    // maxWidth: 300,
+    // maxHeight: 300
+    //}
+
+  }
+};
+
+// variables for time-animations
+var flash = false;
+var flashCounter = 0;
+var editing = false;
+var editMode = false;
+
+var showImageTest = false;
+var alpha = 0;
+
+var shutterSound;
+var galleryElement;
+
+//gallery part
+var currentSelectedImageId = undefined;
+
+//bools
+var selectedImageMode = false;
+var contrastSliderOn = false;
+
+let invertVar =250;
+let posneg =-1;
+
+let sketch = function(p) {
+/*
+  p.preload = function(){
+    for (var i=0; i<5; i++) {
+      //imgs[i] = p.loadImage("games/natur/img/img1"+".png"); 
+    }
+    
+  } */
+
+
+  p.setup = function() {
+    w = window.innerWidth - 60;;
+    h = window.innerHeight - 60;
+
+    captureHeight = w / 3 * 4;
+
+    canvas = p.createCanvas(w, captureHeight);
+    //canvas.parent("game-content");
+    p.background(255,0,0);
+    //capture = p.createCapture(p.VIDEO);
+    capture = p.createCapture(options);
+    // capture.size(document.querySelector('body').clientWidth, document.querySelector('body').clientHeight);
+    capture.hide();
+
+    galleryElement = p.select('#img2');
+   // galleryElement.mouseClicked(p.test);
+    
+
+    shutterSound = p.loadSound('games/natur/src/shutter.wav');
+
+    /*backBtn = p.createButton('back');
+    backBtn.parent('toolbar');
+    backBtn.mousePressed(p.goToPhotoView);
+    backBtn.addClass('myButton');
+    backBtn.id('backBtn');
+
+
+    galleryBtn = p.createButton('gallery');
+    galleryBtn.parent('toolbar');
+    galleryBtn.mousePressed(p.activateGallery);
+    galleryBtn.addClass('myButton');
+    galleryBtn.id('galleryBtn');
+
+
+    shootBtn = p.createButton('shoot pic');
+    shootBtn.parent('toolbar');
+    shootBtn.mousePressed(p.shoot);
+    shootBtn.addClass('myButton');
+    shootBtn.id('shootBtn');
+
+
+    newCaptureBtn = p.createButton('photo');
+    newCaptureBtn.parent('toolbar');
+    newCaptureBtn.mousePressed(p.newCapture);
+    newCaptureBtn.addClass('myButton');
+    newCaptureBtn.id('newCaptureBtn');
+    document.getElementById("newCaptureBtn").style.visibility =  "hidden";
+
+    invertBtn = p.createButton('invert pic');
+    invertBtn.parent('game-toolbar');
+    invertBtn.mousePressed(p.invert);
+    invertBtn.addClass('myButton');
+    invertBtn.id('invertBtn');
+    document.getElementById("invertBtn").style.visibility =  "hidden";
+
+    saveBtn = p.createButton('save');
+    saveBtn.parent('toolbar');
+    saveBtn.mousePressed(p.saveArtwork);
+    saveBtn.addClass('myButton');
+    saveBtn.id('saveBtn');
+    document.getElementById("saveBtn").style.visibility =  "hidden";
+ 
+    selectImageBtn = p.createButton('SELECT');
+    selectImageBtn.parent('toolbar');
+    selectImageBtn.mousePressed(p.selectedImageMode);
+    selectImageBtn.addClass('myButton');
+    selectImageBtn.id('selectImageBtn');
+    document.getElementById("selectImageBtn").style.visibility =  "hidden"; 
+    
+    //CONTRAST PART
+    invertBtn = p.createButton('');
+    invertBtn.parent('contrast-container');
+    invertBtn.mousePressed(p.contrastFunction);
+    invertBtn.addClass('toggle-button');
+    invertBtn.id('contrast-button');
+    //document.getElementById("contrast-button").style.visibility =  "hidden";
+
+    
+    
+    slider1.id('contrast-range');
+    slider1.addClass('rotatedSlider');
+    slider1.parent('contrast-container');
+    document.getElementById("contrast-range").style.visibility =  "hidden";*/
+
+    slider1 = p.createSlider(0, 255, 100);
+    slider1.id('contrast-range');
+    slider1.addClass('rotatedSlider');
+    slider1.parent('contrast-container');
+    document.getElementById("contrast-range").style.visibility =  "hidden";
+    
+    
+
+  };
+
+  p.draw = function() {
+
+      //p.background(0);
+      p.image(capture, 0, 0, w, captureHeight);
+      p.filter(p.GRAY);
+  }   
+
+
+  p.writeSmth = function() {
+
+    //p.background(0);
+    console.log("YEAHHH");
+}  
+
+  
+  p.contrastFunction = function(){
+    contrastSliderOn=!contrastSliderOn;
+    if(contrastSliderOn){
+      document.getElementById("contrast-range").style.visibility =  "visible";
+    } else{
+      document.getElementById("contrast-range").style.visibility =  "hidden";
+    }
+    
+  }
+
+  p.draw = function() {
+    //wenn nicht editiert wird
+    if (!editing) {
+      //p.background(0);
+      p.image(capture, 0, 0, w, captureHeight);
+      p.filter(p.GRAY);
+      if (picture != null) {
+        //p.image(picture, 0, 0, 300, 300);
+      }
+       capture.get(0, 0, w, captureHeight);
+      if (editMode) {
+        //capture.get(0, 0, w, captureHeight);
+        capture.loadPixels();
+        picture = capture.get(0, 0, 300, 300);
+        picture.loadPixels();
+        editMode=false;
+        p.print(picture);
+      }
+
+      if(flash && flashCounter < 6){
+        flashCounter+=1;
+        p.fill(0);
+        p.rect(0,0,w,h);
+      }
+
+      //picture.resize(w, captureHeight);
+      slider1.changed(p.changeContrast);
+    }
+
+    if(showImageTest && alpha < 255){
+       p.background(255);
+      p.tint(255, alpha);
+      p.image(picture, 0,0);
+      alpha+=1;
+    }
+
+    if(selectedImageMode==true){
+      p.image(imgs[currentSelectedImageId],0,0,w,h);
+    }   
+  };
+  p.changeContrast = function(){
+    console.log("contrast");
+    editing = true;
+    editMode=true;
+    selectedImageMode = false;
+
+    picture.loadPixels();
+  //p.loadPixels();
+   for (let x = 0; x < picture.width; x++) {
+    for (let y = 0; y < picture.height; y++) {
+      // Calculate the 1D location from a 2D grid
+      let loc = (x + y * picture.width) * 4;
+      // Get the R,G,B values from image
+      let r, g, b;
+      r = picture.pixels[loc];
+      // Calculate an amount to change brightness based on proximity to the mouse
+      let maxdist = 50;
+     // let d = dist(x, y, mouseX, mouseY);
+       let adjustbrightness = slider1.value();
+     
+      r += adjustbrightness;
+      
+      // Constrain RGB to make sure they are within 0-255 color range
+      r = p.constrain(r, 0, 255);
+      // Make a new color and set pixel in the window
+      //color c = color(r, g, b);
+      let pixloc = (y * picture.width + x) * 4;
+      picture.pixels[pixloc] = invertVar +r*posneg;
+      picture.pixels[pixloc + 1] = invertVar +r*posneg;
+      picture.pixels[pixloc + 2] = invertVar+ r*posneg;
+      picture.pixels[pixloc + 3] = 255;
+    }
+  }
+  picture.updatePixels();
+  p.image(picture,0,0,w, captureHeight);
+  }
+
+  p.goToPhotoView = function(){
+    //start camera
+    capture.play();
+    editing=false;
+    //hide buttons
+    
+    
+    //hide gallery
+    document.getElementById("gallery").classList.remove("gallery-animation");
+    document.getElementById("gallery").classList.add("gallery-animation-reverse");
+    
+    //show buttons
+    document.getElementById("galleryBtn").style.visibility =  "visible";
+    document.getElementById("shootBtn").style.visibility =  "visible";
+    document.getElementById("gallery").title="";
+  }
+
+  p.activateGallery = function(){
+    //stop filming
+    capture.pause();
+    editing=true;
+    
+    document.getElementById("gallery").classList.remove("gallery-animation-reverse");
+    //hide gallery Button
+    document.getElementById("galleryBtn").style.visibility =  "hidden";
+    document.getElementById("shootBtn").style.visibility =  "hidden";
+    document.getElementById("saveBtn").style.visibility =  "hidden";   
+    //show gallery canvas
+    document.getElementById("gallery").style.visibility =  "visible";
+    document.getElementById("selectImageBtn").style.visibility =  "hidden";  
+    document.getElementById("gallery").classList.add("gallery-animation");
+    //show buttons
+    
+    
+    
+  }
+
+
+  p.selectedImageMode = function(){
+    
+    //hide buttons
+    document.getElementById("selectImageBtn").style.visibility =  "hidden"; 
+   //hide gallery
+    document.getElementById("gallery").classList.remove("gallery-animation");
+    document.getElementById("gallery").classList.add("gallery-animation-reverse");
+
+    //show buttons
+    document.getElementById("galleryBtn").style.visibility =  "visible";
+    document.getElementById("shootBtn").style.visibility =  "visible";
+    document.getElementById("invertBtn").style.visibility =  "visible";
+    //activate image editing
+
+    currentSelectedImageId = document.getElementById("gallery").title;
+    if(currentSelectedImageId.length>0){
+      selectedImageMode = true;
+      var currentImage = imgs[currentSelectedImageId];
+      currentImage.loadPixels();
+      picture = currentImage.get();
+    }
+    
+    
+    //
+    
+  }
+
+  p.shoot = function() {
+    if (capture.loadedmetadata) {
+
+    }
+    capture.loadPixels();
+    picture = capture.get();
+    //picture.resize(w, captureHeight);
+    p.print(picture);
+    capture.pause();
+    flash = true;
+    shutterSound.play();
+    document.getElementById("shootBtn").style.visibility ="hidden";
+    document.getElementById("newCaptureBtn").style.visibility =  "visible";
+    document.getElementById("invertBtn").style.visibility =  "visible";
+    document.getElementById("saveBtn").style.visibility =  "visible";
+  };
+
+
+  p.newCapture = function() {
+    picture = null;
+    editMode = false;
+    editing = false;
+    alpha = 0;
+    flashCounter = 0;
+    showImageTest = false;
+    flash = false;
+    capture.play();
+
+    document.getElementById("shootBtn").style.visibility ="visible";
+    document.getElementById("newCaptureBtn").style.visibility =  "hidden";
+    document.getElementById("invertBtn").style.visibility =  "hidden";
+    document.getElementById("saveBtn").style.visibility =  "hidden";
+  };
+
+  p.saveArtwork = function() {
+  picture.save();
+  };
+
+
+  p.invert = function() {
+    editing = true;
+    editMode=true;
+    selectedImageMode = false;
+  //p.image(picture, 0,0,700,700); 
+    picture.loadPixels();
+    for (let x = 0; x < picture.width; x++) {
+    for (let y = 0; y < picture.height; y++) {
+      // Calculate the 1D location from a 2D grid
+      let loc = (x + y * picture.width) * 4;
+      // Get the R,G,B values from image
+      let r, g, b;
+      r = picture.pixels[loc];
+
+      // Calculate an amount to change brightness based on proximity to the mouse
+      let maxdist = 50;
+     // let d = dist(x, y, mouseX, mouseY);
+       let adjustbrightness = 0;
+     
+      r += adjustbrightness;
+      
+      // Constrain RGB to make sure they are within 0-255 color range
+      r = p.constrain(r, 0, 255);
+      // Make a new color and set pixel in the window
+      //color c = color(r, g, b);
+      let pixloc = (y * picture.width + x) * 4;
+      picture.pixels[pixloc] = 255 -r;
+      picture.pixels[pixloc + 1] = 255 -r;
+      picture.pixels[pixloc + 2] = 255- r;
+      picture.pixels[pixloc + 3] = 255;
+    }
+  }
+   picture.updatePixels();
+   picture.resize(w,0);
+    // p.image(picture, 0,0);  
+    p.background(0);
+    showImageTest = true;
+    alpha = 0;
+
+    
+  };
+
+  
+  
+  
+  p.blechserie = function() {
+    editing = true;
+    editMode=true;
+    //p.background(255);
+    picture.resize(200, 0);
+    //p.image(picture, 0, 0);
+    var mouseXFactor = p.map(300, 0, w, 0.05, 1);
+    var mouseYFactor = p.map(300, 0, captureHeight, 0.05, 1);
+    //scale(10);
+    //p.translate(-p.mouseX, -captureHeight / 3);
+
+    //MAKE IMAGE SMALLER???
+
+    p.push();
+    for (var gridX = 0; gridX < w; gridX=gridX+10) {
+      for (var gridY = 0; gridY < captureHeight; gridY=gridY+10) {
+        // grid position + tile size
+        var tileWidth = w / w;
+        var tileHeight = captureHeight / captureHeight;
+        var posX = tileWidth * gridX;
+        var posY = tileHeight * gridY;
+
+        // get current color
+
+        //picture.loadPixels();
+        var c = p.color(p.get(gridX, gridY));
+        // greyscale conversion
+        var greyscale = p.round(p.red(c) * 0.222 + p.green(c) * 0.707 + p.blue(c) * 0.071);
+        p.print("        ", p.red(c));
+        // greyscale to ellipse area
+        let color1 = 0;
+        let color2 = 250;
+        let currentColor = 0;
+        let currentAlpha = 0;
+        if (greyscale >= 110) {
+          currentColor = color1;
+          currentAlpha = color2;
+        } else {
+          currentColor = color2;
+          currentAlpha = color1;
+        }
+        p.fill(currentColor, currentAlpha);
+        
+        p.noStroke();
+        var r2 = 1.1284 * p.sqrt(tileWidth * tileWidth * (1 - greyscale / 255));
+        r2 *= mouseXFactor * 3;
+        p.rectMode(p.CENTER);
+        //rect(posX, posY, r2, r2);
+         p.ellipse(posX, posY, tileWidth * 1.8, tileWidth * 1.8); 
+        //p.print("        ",posX, posY,tileWidth * 1.8,currentColor,currentAlpha);
+      }
+    }
+    p.pop();
+
+  };
+
+
+ 
+};
+
+let myp5 = new p5(sketch, 'game-content');
---
+++ 210314_Boch/games/stempel/stempel.js
@@ -0,0 +1,184 @@
+//based on the original example on objects 
+//https://p5js.org/examples/objects-objects.html
+
+
+// responsive 
+
+
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+
+var forms = [];
+var x = 0;
+var y = 0;
+var tileNr = 4;
+var tileNrY = 4;
+var tileSz = 0;
+var counterFor = 0;
+var color_bgd = [10, 35, 50, 75, 100, 150, 190, 220, 255];
+var this_color = 0;
+var color_step = 25;
+var clientWidth;
+var cnv;
+
+var clientHeight;
+var offY = 0.15;
+var gameCanvasWidth,gameCanvasHeight;
+
+
+function setup() {
+    //  clientWidth = document.getElementById('stempelspiel').clientWidth;
+    //  clientHeight = document.getElementById('stempelspiel').clientHeight * 0.7;
+
+    clientWidth = window.innerWidth - 60;
+    clientHeight = window.innerHeight - 60;
+
+
+    background(255);
+    gameCanvasWidth = clientWidth;
+
+    if (window.innerWidth > 750) {
+        tileNr = int(gameCanvasWidth / 85);
+    } else {
+        tileNr = int(gameCanvasWidth / 70);
+    }
+
+    tileSz = clientWidth / tileNr;
+    tileNrY = floor(clientHeight / tileSz) - 1;
+    gameCanvasHeight = tileNrY * tileSz;
+
+    cnv = createCanvas(clientWidth+1, clientHeight);
+    cnv.parent("game-content");
+
+    for (var i = 0; i < tileNrY; i++) {
+        for (var j = 0; j < tileNr; j++) {
+            x = tileSz * j;
+            y = tileSz * i;
+            forms[counterFor] = new Forms(x, y);
+            counterFor = counterFor + 1;
+        }
+    }
+}
+
+window.onresize = function () {
+
+    clientWidth = window.innerWidth - 60;
+    clientHeight = window.innerHeight - 60;
+
+    gameCanvasWidth = clientWidth;
+
+    if (window.innerWidth > 750) {
+        tileNr = int(gameCanvasWidth / 85);
+    } else {
+        tileNr = int(gameCanvasWidth / 70);
+    }
+
+    tileSz = clientWidth / tileNr;
+    tileNrY = floor(clientHeight / tileSz) - 1;
+    gameCanvasHeight = tileNrY * tileSz;
+    for (var i = 0; i < tileNrY; i++) {
+        for (var j = 0; j < tileNr; j++) {
+            x = tileSz * j;
+            y = tileSz * i;
+            forms[counterFor] = new Forms(x, y);
+            counterFor = counterFor + 1;
+        }
+    }
+}
+
+
+function draw() {
+    background(250);
+    for (var i = 0; i < forms.length; i++) {
+        forms[i].display();
+    }
+
+    for (var j = 0; j < tileSz * (tileNr+1); j = j + tileSz) {
+        for (var k = 0; k < tileSz * (tileNrY + 1); k = k + tileSz) {
+            stroke(200);
+            strokeWeight(1);
+            line(j, 0, j, gameCanvasHeight);
+            line(0, k, gameCanvasWidth, k);
+        }
+    }
+
+    touchabfrage();
+
+    
+}
+
+function touchabfrage() {
+
+    if (touches.length != 0) {
+        for (var i = 0; i < forms.length; i++) {
+            var antwort = pointRect(touches[0].x, touches[0].y, forms[i].x, forms[i].y, tileSz, tileSz);
+
+            if (antwort) {
+                if (forms[i].color > this_color) {
+                    //Check color
+                    if (this_color + color_step < 255) {
+                        this_color = this_color + color_step;
+                    } else {
+                        this_color = 255;
+                    }
+
+                    noStroke();
+
+                    forms[i].color = this_color;
+                }
+            }
+            if(this_color==255){
+                document.getElementById("stamp").classList.add('pulse');
+            }
+        }
+    }
+}
+
+function stempelkissen() {
+    this_color = 0;
+    document.getElementById("stamp").classList.remove('pulse');
+}
+
+function pointRect(px, py, rx, ry, rw, rh) {
+
+    // is the point inside the rectangle's bounds?
+    if (px >= rx && // right of the left edge AND
+        px <= rx + rw && // left of the right edge AND
+        py >= ry && // below the top AND
+        py <= ry + rh) { // above the bottom
+        return true;
+    }
+    return false;
+}
+
+//reset colors
+function resetColors() {
+    for (var i = 0; i < forms.length; i++) {
+        forms[i].color = 255;
+    }
+    this_color = 0;
+}
+
+function saveArtwork() {
+    var to_save = get(0, clientHeight * offY, clientWidth, clientWidth);
+    //to_save.save("myComposition");
+    saveCanvas(cnv, 'myCanvas', 'jpg');
+}
+
+class Forms {
+    constructor(tx, ty) {
+        this.x = tx;
+        this.y = ty;
+        this.color = 255;
+    }
+
+    display() {
+        noStroke();
+        fill(this.color);
+        rect(this.x, this.y, tileSz, tileSz);
+        fill(255);
+        ellipse(this.x + tileSz / 2, this.y + tileSz / 2, tileSz - 10, tileSz - 10);
+        //console.log(this.x,this.y);
+    }
+}
